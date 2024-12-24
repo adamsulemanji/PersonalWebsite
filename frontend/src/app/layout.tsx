cb679f9589e96df6df36ecede7147b984030436f
adamsulemanji <84648379+adamsulemanji@users.noreply.github.com>
2024-12-23 23:31:31 -0600

    Add site webmanifest and update metadata for improved PWA support; remove old favicon manifest

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,7 +1,4 @@
-"use client";
-
 import React from "react";
-import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "../styles/globals.css";
 
@@ -23,35 +20,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/favicons/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicons/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicons/favicon-16x16.png"
-        />
-        <link rel="manifest" href="/favicons/site.webmanifest" />
-        <link
-          rel="mask-icon"
-          href="/favicons/safari-pinned-tab.svg"
-          color="#5bbad5"
-        />
-        <meta name="msapplication-TileColor" content="#da532c" />
-        <meta name="theme-color" content="#ffffff" />
-      </head>
       <body
-        className={`${geistMono.variable} ${geistMono.variable} antialiased`}
+        className={`${geistMono.variable} ${geistMono.variable} antialiased accent-bg accent-text`}
       >
         {children}
       </body>
---
+++ frontend/src/app/metadata.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon-16x16.png",
+    apple: "/apple-touch-icon.png",
+    other: [
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "32x32",
+        url: "/favicon-32x32.png",
+      },
+      {
+        rel: "manifest",
+        url: "/site.webmanifest",
+      },
+      {
+        rel: "mask-icon",
+        url: "/safari-pinned-tab.svg",
+        color: "#5bbad5",
+      },
+    ],
+  },
+  themeColor: "#ffffff",
+  manifest: "/site.webmanifest",
+};
